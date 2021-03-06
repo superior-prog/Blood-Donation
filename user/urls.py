2841fdd0ecf5e2850f9649db11117959a83dba12
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-06 23:08:34 +0600

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -4,28 +4,27 @@ from .views import *
 
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name="register"),
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
+    path('register', register_view, name="register"),
     path('user/<str:pk>', profile_view, name="profile"),
     path('user/edit-profile/', edit_profile_view, name="edit-profile"),
     path('user/settings/', settings_view, name="settings"),
-    path('donors/', donor_view, name="donors"),
-    path('contact/', contact_view, name="contact"),
-    path('notifications/', notification_view, name="notification"),
+    path('donors', donor_view, name="donors"),
+    path('contact', contact_view, name="contact"),
 
-    path('reset-password/',
+    path('reset-password',
          auth_views.PasswordResetView.as_view(template_name="password-reset.html"),
          name="reset-password"),
-    path('reset-password-sent/',
+    path('reset-password-sent',
          auth_views.PasswordResetDoneView.as_view(template_name="password-reset-sent.html"),
          name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/',
+    path('reset/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(template_name="password-reset-form.html"),
          name="password_reset_confirm"),
 
-    path('reset-password-complete/',
+    path('reset-password-complete',
          auth_views.PasswordResetCompleteView.as_view(template_name="password-reset-done.html"),
          name="password_reset_complete"),
 ]
