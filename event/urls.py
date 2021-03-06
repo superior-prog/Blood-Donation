a752246d31ac81f738321cebcfff33201704e1a5
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-06 23:09:28 +0600

    Update urls.py

--- event/urls.py
+++ event/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', event_home_view, name='event-home'),
-    path('create-new-event/', create_event_view, name="create-event"),
-    path('<str:pk>/', event_detail_view, name='event-detail'),
-    path('edit/<str:pk>/', edit_event_view, name='edit-event'),
-    path('delete/<str:pk>/', delete_event_view, name='delete-event'),
+    path('create-new-event', create_event_view, name="create-event"),
+    path('<str:pk>', event_detail_view, name='event-detail'),
+    path('edit/<str:pk>', edit_event_view, name='edit-event'),
+    path('delete/<str:pk>', delete_event_view, name='delete-event'),
 ]
