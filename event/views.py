a8a904ab66606ffedd5dd72a58a526dda9e20477
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-06 23:09:32 +0600

    Update views.py

--- event/views.py
+++ event/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect
-from .forms import CreateEventForm
-from .models import EventModel
+from .forms import CreateEventForm, InvitationForm
+from .models import EventModel, EventInvitationModel
+from user.models import User, ProfileModel
 
 
+@login_required(login_url='login')
 def event_home_view(request):
     event_list = EventModel.objects.all()
 
@@ -19,6 +21,7 @@ def event_home_view(request):
     return render(request, 'event/index.html', context)
 
 
+@login_required(login_url='login')
 def create_event_view(request):
     task = "Create"
     form = CreateEventForm()
@@ -43,6 +46,7 @@ def create_event_view(request):
     return render(request, 'event/create_event.html', context)
 
 
+@login_required(login_url='login')
 def edit_event_view(request, pk):
     task = "Update"
     event = EventModel.objects.get(id=pk)
@@ -66,22 +70,80 @@ def edit_event_view(request, pk):
     return render(request, 'event/create_event.html', context)
 
 
+@login_required(login_url='login')
 def delete_event_view(request, pk):
-    # blog = BlogModel.objects.get(slug=slug)
-    # if request.method == 'POST':
-    #     blog.delete()
-    #     return redirect('blogs', request.user.id)
+    event = EventModel.objects.get(id=pk)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('event-home')
 
     context = {
-        # 'item': blog,
+        'item': event,
     }
-    return render(request, 'delete-blog.html', context)
+    return render(request, 'event/delete_event.html', context)
 
 
+@login_required(login_url='login')
 def event_detail_view(request, pk):
     event = EventModel.objects.get(id=pk)
 
+    location_link = "https://maps.google.com/maps?width=100%25&amp;height=450&amp;hl=en&amp;q="
+
+    if event.location is not None:
+        locations = event.location.split(' ')
+        location_link = "https://maps.google.com/maps?width=100%25&amp;height=450&amp;hl=en&amp;q="
+
+        for location in locations:
+            location_link += location + "%20"
+
+        location_link += "&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+
+    users = User.objects.all()
+    event_invitation = EventInvitationModel.objects.filter(event=event)
+    # print(event_invitation)
+
+    invited_people = []
+    uninvited_people = []
+
+    for person in event_invitation:
+        invited_people.append(person.member)
+
+    for person in users:
+        if person not in invited_people and person != event.host and not person.is_staff:
+            uninvited_people.append(person)
+
+    # print(invited_people)
+    # print(uninvited_people)
+    # print(event.host)
+
+    # invitation_form = InvitationForm()
+    # if request.method == 'POST':
+    #     invitation_form = InvitationForm(request.POST)
+    #     if invitation_form.is_valid():
+    #         invitation = invitation_form.save(commit=False)
+
+    if request.GET.get('dltInvite'):
+        personID = request.GET.get('dltPersonID')
+        person = User.objects.get(pk=personID)
+
+        invitation = EventInvitationModel.objects.get(event=event, member=person)
+        invitation.delete()
+        return redirect('event-detail', event.id)
+
+    if request.GET.get('personID'):
+        personID = request.GET.get('personID')
+        person = User.objects.get(pk=personID)
+
+        invitation = EventInvitationModel(event=event, member=person)
+        invitation.save()
+        return redirect('event-detail', event.id)
+
     context = {
         'event': event,
+        'location_link': location_link,
+        # 'profiles': profiles,
+        # 'invitation_form': invitation_form,
+        'invited_people': invited_people,
+        'uninvited_people': uninvited_people,
     }
     return render(request, 'event/event_detail.html', context)
