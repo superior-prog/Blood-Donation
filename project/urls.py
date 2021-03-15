486c5ca4e5c72e7f4c55cd5fb9b3769790c5fbd8
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-15 14:57:10 +0600

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -5,12 +5,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('user.urls')),
+    path('accounts/', include('user.urls')),
     path('', include('blood_request.urls')),
     path('event/', include('event.urls')),
     path('notifications', include('notification_control.urls')),
 
-    # path('oauth/', include('social_django.urls', namespace='social')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
